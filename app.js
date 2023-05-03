0e8232e9c080cc04e4e394f6aafc893c3211f499
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 23:11:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 function checkWallet(seedPhrase) {
   const mnemonic = seedPhrase;
-  const seed = bitcoin.bip39.mnemonicToSeedSync(mnemonic);
-  const root = bitcoin.bip32.fromSeed(seed);
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+  const root = bip32.fromSeed(seed);
   const path = "m/44'/145'/0'/0/0";
   const child = root.derivePath(path);
   const keyPair = child.toWIF();
 
-  const { address } = bitcoin.payments.p2pkh({
+  const { address } = payments.p2pkh({
     pubkey: child.publicKey,
-    network: bitcoin.networks.bitcoin
+    network: networks.bitcoin
   });
 
-  const apiKey = "your_blockchair_api_key";
-  const url = `https://api.blockchair.com/bitcoin/dashboards/address/${address}?key=${apiKey}`;
-
-  fetch(url)
+  const apiUrl = `https://explorer.e.cash/v1/address/${address}`;
+  fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      const balance = data.data[address].address.balance;
-      console.log(`Adres: ${address}, Saldo: ${balance}`);
+      const balance = data.balance;
+      const txCount = data.transactionCount;
+      console.log(`Adres: ${address}, Saldo: ${balance}, Liczba transakcji: ${txCount}`);
     })
     .catch((error) => console.error(error));
 }
 
 // Get reference to the crack button
 const crackButton = document.getElementById("crack-button");
-
-// Get reference to the clear button
-const clearButton = document.getElementById("clear-button");
 
 // Add click event listener to the crack button
 crackButton.addEventListener("click", () => {
@@ -37,6 +33,9 @@ crackButton.addEventListener("click", () => {
   // Call the checkWallet function with the seed phrase as an argument
   checkWallet(seedPhrase);
 });
+
+// Get reference to the clear button
+const clearButton = document.getElementById("clear-button");
 
 // Add click event listener to the clear button
 clearButton.addEventListener("click", () => {
