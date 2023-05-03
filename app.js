e05c599f42570632176fee537b675ad85fdbcff0
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 21:19:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,57 @@
+// Select word input elements
 const wordInputs = document.querySelectorAll('.word-input');
 
-// Add paste and clear icons for each input
-wordInputs.forEach(input => {
-  const pasteIcon = document.createElement('i');
-  pasteIcon.classList.add('fas', 'fa-paste', 'input-icon');
-  input.parentNode.appendChild(pasteIcon);
-  pasteIcon.addEventListener('click', () => {
-    navigator.clipboard.readText().then(text => input.value = text);
-  });
+// Select generate button element
+const generateButton = document.getElementById('generate-seeds');
 
-  const clearIcon = document.createElement('i');
-  clearIcon.classList.add('fas', 'fa-times-circle', 'input-icon');
-  input.parentNode.appendChild(clearIcon);
-  clearIcon.addEventListener('click', () => input.value = '');
-});
-
-// Generate seed phrases
-function generateSeeds() {
-  // Get word inputs
-  const words = [];
-  wordInputs.forEach(input => words.push(input.value.trim()));
-
-  // Filter out empty inputs
-  const nonEmptyWords = words.filter(word => word !== '');
-
-  // Generate combinations
-  const combinations = getCombinations(nonEmptyWords);
-
-  // Render combinations
-  renderCombinations(combinations);
-}
+// Select result textarea element
+const resultTextarea = document.getElementById('result');
 
 // Register event listeners
-document.getElementById('generate-seeds').addEventListener('click', generateSeeds);
-wordInputs.forEach(input => input.addEventListener('input', generateSeeds));
+generateButton.addEventListener('click', generateSeeds);
+wordInputs.forEach(input => {
+    input.addEventListener('input', generateSeeds);
+    input.nextElementSibling.addEventListener('click', pasteFromClipboard);
+    input.nextElementSibling.nextElementSibling.addEventListener('click', clearInput);
+});
+
+/**
+ * Generate seed phrases from the word inputs
+ */
+function generateSeeds() {
+    const words = [];
+    for (let i = 1; i <= 12; i++) {
+        const input = document.getElementById(`word-${i}`);
+        const inputWords = input.value.trim().split(' ');
+        if (inputWords.length > 1) {
+            words.push(inputWords[Math.floor(Math.random() * inputWords.length)]);
+        } else if (inputWords.length === 1) {
+            words.push(inputWords[0]);
+        }
+    }
+    const numSeeds = document.getElementById('num-seeds').value;
+    const seeds = Cain.generateSeedPhrases(words, numSeeds);
+    resultTextarea.value = seeds.join('\n');
+}
+
+/**
+ * Paste text from clipboard to a given input
+ * @param {Event} event 
+ */
+function pasteFromClipboard(event) {
+    const input = event.target.previousElementSibling;
+    navigator.clipboard.readText().then(text => {
+        input.value = text.trim();
+        generateSeeds();
+    });
+}
+
+/**
+ * Clear the value of a given input
+ * @param {Event} event 
+ */
+function clearInput(event) {
+    const input = event.target.previousElementSibling.previousElementSibling;
+    input.value = '';
+    generateSeeds();
+}
