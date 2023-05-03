db4ab58dfdddce5d6359b8575ec65e7b37ae540c
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 20:34:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,91 +1,65 @@
-const wordInputs = document.querySelectorAll(".word-input");
+// Get form inputs
+const wordInputs = document.querySelectorAll('.word-input');
 
-function getWords() {
-  const words = [];
-  for (let i = 0; i < wordInputs.length; i++) {
-    const word = wordInputs[i].value.trim();
-    if (word !== "") {
-      words.push(word);
+// Generate all combinations of words
+function generateCombinations(words) {
+  const result = [];
+  function helper(arr, i) {
+    for (let j = 0; j < words[i].length; j++) {
+      const a = arr.slice(0);
+      a.push(words[i][j]);
+      if (i === words.length - 1) {
+        result.push(a.join(' '));
+      } else {
+        helper(a, i + 1);
+      }
     }
   }
-  return words;
+  helper([], 0);
+  return result;
 }
 
-function generateCombinations() {
-  const words = getWords();
-  const combinations = getCombinations(words);
-  const combinationList = document.getElementById("combinations");
-  combinationList.innerHTML = "";
-  for (let i = 0; i < combinations.length && i < 100000; i++) {
-    const combination = combinations[i];
-    const listItem = document.createElement("li");
-    listItem.innerText = combination;
-    combinationList.appendChild(listItem);
-  }
-  showPages();
-}
-
-function showPages() {
-  const combinations = document.getElementById("combinations");
-  const pageContainer = document.getElementById("pages");
-  const numPages = Math.ceil(combinations.children.length / 100000);
-  pageContainer.innerHTML = "";
-  for (let i = 0; i < numPages; i++) {
-    const pageNumber = i + 1;
-    const pageButton = document.createElement("button");
-    pageButton.innerText = pageNumber;
-    pageButton.onclick = function () {
-      showPage(pageNumber);
-    };
-    pageContainer.appendChild(pageButton);
-  }
-}
-
-function showPage(pageNumber) {
-  const combinations = document.getElementById("combinations");
-  const startIndex = (pageNumber - 1) * 100000;
-  const endIndex = startIndex + 100000;
-  for (let i = 0; i < combinations.children.length; i++) {
-    if (i >= startIndex && i < endIndex) {
-      combinations.children[i].style.display = "block";
+// Generate pagination links
+function generatePaginationLinks(currentPage, pageCount) {
+  const pagination = document.getElementById('pagination');
+  pagination.innerHTML = '';
+  const maxLinks = 7;
+  let startLink = Math.max(1, currentPage - Math.floor(maxLinks / 2));
+  let endLink = Math.min(pageCount, startLink + maxLinks - 1);
+  startLink = Math.max(1, endLink - maxLinks + 1);
+  for (let i = startLink; i <= endLink; i++) {
+    const link = document.createElement('button');
+    link.innerText = i;
+    if (i === currentPage) {
+      link.disabled = true;
     } else {
-      combinations.children[i].style.display = "none";
+      link.addEventListener('click', () => showPage(i));
     }
+    pagination.appendChild(link);
   }
 }
 
+// Display page
+function showPage(pageNumber) {
+  const combinations = document.getElementById('combinations');
+  combinations.innerHTML = '';
+  const pageSize = 50;
+  const start = (pageNumber - 1) * pageSize;
+  const end = pageNumber * pageSize;
+  const pageData = window.allCombinations.slice(start, end);
+  const p = document.createElement('p');
+  p.innerText = pageData.join('\n');
+  combinations.appendChild(p);
+  generatePaginationLinks(pageNumber, Math.ceil(window.allCombinations.length / pageSize));
+}
+
+// Generate seeds
 function generateSeeds() {
-  const words = getWords();
-  const combinations = getCombinations(words);
-  const seeds = [];
-  for (let i = 0; i < combinations.length; i++) {
-    const seed = btoa(combinations[i]);
-    seeds.push(seed);
-  }
-  const seedList = document.getElementById("seeds");
-  seedList.innerHTML = "";
-  for (let i = 0; i < seeds.length; i++) {
-    const seedItem = document.createElement("div");
-    seedItem.className = "seed-item";
-    const seedValue = document.createElement("span");
-    seedValue.className = "seed-value";
-    seedValue.innerText = seeds[i];
-    const copyButton = document.createElement("button");
-    copyButton.className = "copy-button";
-    copyButton.innerText = "Copy";
-    copyButton.onclick = function () {
-      const tempInput = document.createElement("input");
-      tempInput.value = seeds[i];
-      document.body.appendChild(tempInput);
-      tempInput.select();
-      document.execCommand("copy");
-      document.body.removeChild(tempInput);
-    };
-    seedItem.appendChild(seedValue);
-    seedItem.appendChild(copyButton);
-    seedList.appendChild(seedItem);
-  }
+  const words = Array.from(wordInputs).map(input => input.value.split(' '));
+  window.allCombinations = generateCombinations(words);
+  showPage(1);
 }
 
-document.getElementById("generate-combinations").onclick = generateCombinations;
-document.getElementById("generate-seeds").onclick = generateSeeds;
+// Register event listeners
+document.getElementById('generate-seeds').addEventListener('click', generateSeeds);
+wordInputs.forEach(input => input.addEventListener('input', generateSeeds));
