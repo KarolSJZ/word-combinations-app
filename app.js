4bbb17b87e2685dca862a2ec3d4c5612dac30b7a
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 23:03:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-function checkWallet(seedPhrase) {
-  const mnemonic = seedPhrase;
-  const seed = bip39.mnemonicToSeedSync(mnemonic);
-  const root = bip32.fromSeed(seed);
-  const path = "m/44'/145'/0'/0/0";
-  const child = root.derivePath(path);
-  const keyPair = child.toWIF();
+// Get reference to the import button
+const importButton = document.getElementById("import");
 
-  const { address } = payments.p2pkh({
-    pubkey: child.publicKey,
-    network: networks.bitcoin
-  });
+// Get reference to the clear button
+const clearButton = document.getElementById("clear");
 
-  const apiKey = "your_blockchair_api_key";
-  const url = `https://api.blockchair.com/bitcoin/dashboards/address/${address}?key=${apiKey}`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const balance = data.data[address].address.balance;
-      console.log(`Adres: ${address}, Saldo: ${balance}`);
-    })
-    .catch((error) => console.error(error));
-}
-
-// Get reference to the crack button
-const crackButton = document.getElementById("crack-button");
-
-// Add click event listener to the crack button
-crackButton.addEventListener("click", () => {
+// Add click event listener to the import button
+importButton.addEventListener("click", () => {
   // Get the seed phrase from the input field
   const seedPhrase = document.getElementById("seed-phrase").value.trim();
 
-  // Call the checkWallet function with the seed phrase as an argument
-  checkWallet(seedPhrase);
-});
+  // Check if the seed phrase is empty
+  if (seedPhrase === "") {
+    alert("Please enter a seed phrase.");
+    return;
+  }
 
-// Get reference to the clear button
-const clearButton = document.getElementById("clear-button");
+  // Check if the seed phrase is valid
+  if (!bitcoin.bip39.validateMnemonic(seedPhrase)) {
+    alert("Invalid seed phrase. Please check your input and try again.");
+    return;
+  }
+
+  // Generate a bitcoin address from the seed phrase
+  const mnemonic = seedPhrase;
+  const seed = bitcoin.bip39.mnemonicToSeedSync(mnemonic);
+  const root = bitcoin.bip32.fromSeed(seed);
+  const path = "m/44'/0'/0'/0/0";
+  const child = root.derivePath(path);
+  const keyPair = child.toWIF();
+  const { address } = bitcoin.payments.p2pkh({
+    pubkey: child.publicKey,
+    network: bitcoin.networks.bitcoin,
+  });
+
+  // Redirect to the generated address page
+  const url = `https://www.blockchain.com/btc/address/${address}`;
+  window.open(url, "_blank");
+});
 
 // Add click event listener to the clear button
 clearButton.addEventListener("click", () => {
