3bc60ad147c759ca21cf334b0471f97b5bb190ff
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 23:18:12 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-function checkWallet(seedPhrase) {
-  const mnemonic = seedPhrase;
-  const seed = bip39.mnemonicToSeedSync(mnemonic);
-  const root = bip32.fromSeed(seed);
-  const path = "m/44'/145'/0'/0/0";
-  const child = root.derivePath(path);
-  const keyPair = child.toWIF();
-
-  const { address } = payments.p2pkh({
-    pubkey: child.publicKey,
-    network: networks.bitcoin
-  });
-
-  const apiUrl = `https://explorer.e.cash/v1/address/${address}`;
-  fetch(apiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-      const balance = data.balance;
-      const txCount = data.transactionCount;
-      console.log(`Adres: ${address}, Saldo: ${balance}, Liczba transakcji: ${txCount}`);
-      // Display the results in the "results" div
-      const resultsDiv = document.getElementById("results");
-      resultsDiv.innerHTML = `<p>Adres: ${address}</p><p>Saldo: ${balance}</p><p>Liczba transakcji: ${txCount}</p>`;
-    })
-    .catch((error) => console.error(error));
-}
-
 // Get reference to the crack button
 const crackButton = document.getElementById("crack-button");
 
@@ -33,8 +6,46 @@ crackButton.addEventListener("click", () => {
   // Get the seed phrase from the input field
   const seedPhrase = document.getElementById("seed-phrase").value.trim();
 
-  // Call the checkWallet function with the seed phrase as an argument
-  checkWallet(seedPhrase);
+  // Check if the seed phrase is empty
+  if (seedPhrase === "") {
+    alert("Please enter a seed phrase.");
+    return;
+  }
+
+  // Check if the seed phrase is valid
+  if (!bitcoin.bip39.validateMnemonic(seedPhrase)) {
+    alert("Invalid seed phrase. Please check your input and try again.");
+    return;
+  }
+
+  // Generate a bitcoin address from the seed phrase
+  const mnemonic = seedPhrase;
+  const seed = bitcoin.bip39.mnemonicToSeedSync(mnemonic);
+  const root = bitcoin.bip32.fromSeed(seed);
+  const path = "m/44'/0'/0'/0/0";
+  const child = root.derivePath(path);
+  const keyPair = child.toWIF();
+  const { address } = bitcoin.payments.p2pkh({
+    pubkey: child.publicKey,
+    network: bitcoin.networks.bitcoin,
+  });
+
+  // Get the balance for the generated address
+  const apiUrl = `https://explorer.e.cash/v1/address/${address}`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      const balance = data.balance;
+      const txCount = data.transactionCount;
+
+      // Display the address and balance
+      const resultsDiv = document.getElementById("results");
+      resultsDiv.innerHTML = `<p>Address: ${address}</p><p>Balance: ${balance} XEC</p><p>Number of Transactions: ${txCount}</p>`;
+    })
+    .catch((error) => {
+      console.error(error);
+      alert("An error occurred while fetching balance. Please try again later.");
+    });
 });
 
 // Get reference to the clear button
@@ -42,10 +53,4 @@ const clearButton = document.getElementById("clear-button");
 
 // Add click event listener to the clear button
 clearButton.addEventListener("click", () => {
-  // Clear the input field
-  document.getElementById("seed-phrase").value = "";
-
-  // Clear the results div
-  const resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = "";
-});
+  // Clear the input field and results
