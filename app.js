5f948633d8401ef84f3fbfb9a13de87d16d9ceb4
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 21:43:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,72 +1,17 @@
-// Clear input when clear icon is clicked
-function clearInput(event) {
-  const input = event.target.previousElementSibling;
-  input.value = "";
-}
-
-// Paste text from clipboard to input when paste icon is clicked
-function pasteToInput(event) {
-  const input = event.target.previousElementSibling;
-  navigator.clipboard.readText().then((text) => {
-    input.value = text;
-  });
-}
-
-// Generate all possible combinations of one word from each input
-function generate(event) {
-  event.preventDefault();
-  const words = [];
-  const seedOutput = document.getElementById("seed-output");
-  const seedOutputTextarea = document.getElementById("seed-output-textarea");
-
-  // Loop through all the word inputs and add them to the words array
-  for (let i = 1; i <= 12; i++) {
-    const input = document.getElementById(`word-${i}`);
-    const inputWords = input.value.split(" ");
-    words.push(inputWords);
-  }
-
-  // Generate all possible combinations of one word from each input
-  const combinations = cartesian(...words).map((c) => c.join(" "));
-
-  // Display the generated seed phrases
-  seedOutputTextarea.value = combinations.join("\n");
-  seedOutput.style.display = "block";
-}
-
-// Copy seed phrases to clipboard when "Copy to Clipboard" button is clicked
-function copyToClipboard(event) {
-  const seedOutputTextarea = document.getElementById("seed-output-textarea");
-  seedOutputTextarea.select();
-  document.execCommand("copy");
-}
-
-// Generate all possible combinations of arrays
-function cartesian(...arrays) {
-  return arrays.reduce((a, b) => {
-    return a.flatMap((x) => {
-      return b.map((y) => {
-        return x.concat(y);
-      });
-    });
-  }, [[]]);
-}
-
-// Add event listeners to paste and clear icons
-const pasteIcons = document.querySelectorAll(".fa-paste");
-pasteIcons.forEach((icon) => {
-  icon.addEventListener("click", pasteToInput);
+document.getElementById('pasteButton').addEventListener('click', () => {
+  // Wklejanie danych ze schowka do pola tekstowego
 });
 
-const clearIcons = document.querySelectorAll(".fa-times-circle");
-clearIcons.forEach((icon) => {
-  icon.addEventListener("click", clearInput);
+document.getElementById('clearButton').addEventListener('click', () => {
+  const inputArea = document.getElementById('inputArea');
+  inputArea.value = '';
 });
 
-// Add event listener to generate button
-const generateButton = document.getElementById("generate-button");
-generateButton.addEventListener("click", generate);
-
-// Add event listener to copy to clipboard button
-const copyButton = document.getElementById("copy-button");
-copyButton.addEventListener("click", copyToClipboard);
+document.getElementById('crackItButton').addEventListener('click', async () => {
+  const inputArea = document.getElementById('inputArea');
+  const seeds = inputArea.value.split('\n');
+  
+  // Tutaj musisz połączyć się z cashtab.com i przeprowadzić operacje na kluczach seed oraz portfelach
+  
+  // Wyniki powinny być wyświetlone w div#results
+});
