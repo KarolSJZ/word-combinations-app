78d71284536b7c4979b3f977e22cb671cfee8558
KarolSJZ <124446117+KarolSJZ@users.noreply.github.com>
2023-05-03 21:50:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-document.getElementById('pasteButton').addEventListener('click', () => {
-  // Wklejanie danych ze schowka do pola tekstowego
+document.getElementById('pasteButton').addEventListener('click', async () => {
+  const clipboardText = await navigator.clipboard.readText();
+  document.getElementById('inputArea').value = clipboardText;
 });
 
 document.getElementById('clearButton').addEventListener('click', () => {
-  const inputArea = document.getElementById('inputArea');
-  inputArea.value = '';
+  document.getElementById('inputArea').value = '';
 });
 
-document.getElementById('crackItButton').addEventListener('click', async () => {
-  const inputArea = document.getElementById('inputArea');
-  const seeds = inputArea.value.split('\n');
-  
-  // Tutaj musisz połączyć się z cashtab.com i przeprowadzić operacje na kluczach seed oraz portfelach
-  
-  // Wyniki powinny być wyświetlone w div#results
+document.getElementById('crackButton').addEventListener('click', () => {
+  // Implement the cracking functionality here.
 });
